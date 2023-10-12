41c4a8e83512471df17552e664a630ad786f7e92
fingerskier <fingerskier@gmail.com>
2023-10-12 14:45:54 -0500

    app skeleton and IPC

---
+++ src/main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+
+  ipcMain.handle('dialog:openFile', async () => {
+    return 'opening file <test>'
+  })
+
+  ipcMain.handle('dialog:ghibbet', async () => {
+    
+    return 'ghibbeting <test>'
+  })
+}
+
+
+app.on('ready', createWindow);
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
